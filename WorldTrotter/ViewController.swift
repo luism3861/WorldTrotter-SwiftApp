cffd0e09c9876ecafae04adb0557d585908e1a2c
luism3861 <eduardo_media@fragua.com.mx>
2020-06-16 13:56:22 -0500

    Initial Commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Luis Eduardo Madina Huerta on 6/16/20.
+//  Copyright © 2020 Luis Eduardo Madina Huerta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
